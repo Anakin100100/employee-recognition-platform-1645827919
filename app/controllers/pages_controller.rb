42b5948a8672fa284fcef5f7a7c626e6cd48cf91
Paweł Biegun <biegunpawel900@gmail.com>
2022-02-28 11:25:05 +0100

    save

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,8 @@
 class PagesController < ApplicationController
-  def index; end
+  def index; 
+    if !employee_signed_in?
+      flash.now[:notice] = 'You have to be logged in to access your account.'
+      redirect_to new_employee_session_path
+    end
+  end
 end
