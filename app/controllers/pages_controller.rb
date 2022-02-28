ab12496ee37d414a030e5e518c6c6bb24840b37a
Paweł Biegun <biegunpawel900@gmail.com>
2022-02-28 12:23:15 +0100

    rubocop refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
-  def index; 
-    if !employee_signed_in?
-      flash.now[:notice] = 'You have to be logged in to access your account.'
-      redirect_to new_employee_session_path
-    end
+  def index
+    return if employee_signed_in?
+
+    # If the user is not signed in they are redirected to sign in page
+    flash.now[:notice] = 'You have to be logged in to access your account.'
+    redirect_to new_employee_session_path
   end
 end
